a0dcec1a5038c6151076edd92da2d10b59969482
Alejandro Trujillo <alejandro.trujilloo@urv.cat>
2024-03-04 15:38:54 +0100

    Initial commit

--- app/src/main/java/com/example/gymfit/MainActivity.kt
+++ app/src/main/java/com/example/gymfit/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Prueba de commit
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
